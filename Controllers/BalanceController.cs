85d8ba3e4448888ef46f3d786c1c76c742ddba89
jens <j.reinartz@traperto.com>
2019-01-12 16:11:32 +0100

    BalanceController Basis

---
+++ Controllers/BalanceController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ColaTerminal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColaTerminal.Controllers
+{
+    [Route("api/[controller]")]
+    public class BalanceController : Controller
+    {
+        public class BalanceInput
+        {
+            public uint amount { get; set; }
+        }
+
+        private readonly traperto_kurtContext dbcontext;
+        public BalanceController(traperto_kurtContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        [HttpPost("[action]")]
+        public bool payment([FromForm] BalanceInput input)
+        {
+            // TODO authentication
+        }
+    }
+}
