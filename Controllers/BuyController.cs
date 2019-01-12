07be3e707ca66d49ae015f00f722fd8a85cd93b0
Niklas Schmidt <n.schmidt@traperto.com>
2019-01-12 17:04:13 +0100

    Reformat Code

--- Controllers/BuyController.cs
+++ Controllers/BuyController.cs
@@ -1,4 +1,5 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Global InconsistentNaming
+
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using ColaTerminal.Models;
@@ -11,11 +12,9 @@ namespace ColaTerminal.Controllers
     {
         public class BuyInput
         {
-            [Required]
-            public uint DrinkId { get; set; }
-            
-            [Required]
-            public string RfId { get; set; }
+            [Required] public uint DrinkId { get; set; }
+
+            [Required] public string RfId { get; set; }
         }
 
         private readonly traperto_kurtContext dbcontext;
@@ -26,16 +25,16 @@ namespace ColaTerminal.Controllers
         }
 
         [HttpPost("[action]")]
-        public ActionResult Buy([FromBody]BuyInput userParam)
+        public ActionResult Buy([FromBody] BuyInput userParam)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(userParam);
             }
-            
+
             var user = dbcontext.User.FirstOrDefault(u => u.RfId == userParam.RfId);
             var drink = dbcontext.Drink.FirstOrDefault(d => d.Id == userParam.DrinkId);
-            
+
             if (user == null || drink == null)
             {
                 return NotFound();
@@ -44,7 +43,7 @@ namespace ColaTerminal.Controllers
             var proceed = new Proceed {UserId = user.Id, DrinkId = drink.Id};
             dbcontext.Proceed.Add(proceed);
             dbcontext.SaveChangesAsync();
-            
+
             return Created($"/proceed/{proceed.Id}", proceed);
         }
     }
