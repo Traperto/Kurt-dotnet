860412f83d75254a0ea49404a8fb25b91e9c47a7
jens <j.reinartz@traperto.com>
2019-01-12 18:47:12 +0100

    Using hinzugefügt

--- Controllers/RefillController.cs
+++ Controllers/RefillController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ColaTerminal.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ColaTerminal.Controllers
