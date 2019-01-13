b6ef6c1cb95cd39aa19636f1120abeef3e2361a8
Sascha Sels <saschasels@Saschas-MBP.fritz.box>
2019-01-13 04:12:45 +0100

    jwt Token sicherer gemacht

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -1,23 +1,57 @@
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using ColaTerminal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using static ColaTerminal.Controllers.LoginController;
 
 public class TokenController : Controller
 {
     private const string SECRET_KEY = "TQvgjeABMPOwCycOqah5EQu5yyVjpmVG";
     public static readonly SymmetricSecurityKey SIGNING_KEY = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET_KEY));
 
-    [HttpGet]
-    [Route("api/Token/{username}/{password}")]
-    public IActionResult Get(string username, string password)
+    private readonly traperto_kurtContext dbcontext;
+
+    public TokenController(traperto_kurtContext dbcontext)
     {
-        if ("123456" == password)
-            return new ObjectResult(GenerateToken(username));
-        else
-            return BadRequest("" + username + password);
+        this.dbcontext = dbcontext;
+    }
+
+    [HttpPost]
+    [Route("api/Token/")]
+    public IActionResult Post([FromBody] LoginInput userParam)
+    {
+        Console.WriteLine(userParam.Password);
+
+        var user = this.dbcontext.User.FirstOrDefault(b => b.UserName == userParam.Username);
+
+        if (user == null)
+        {
+            return NotFound("User could not be found for username: " + userParam.Username);
+        }
+
+        SHA256 sha256Hash = SHA256.Create();
+        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(userParam.Password));
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            builder.Append(bytes[i].ToString("x2"));
+        }
+
+        var hashedPassword = builder.ToString();
+
+
+        if (hashedPassword != user.Password)
+        {
+            return BadRequest("Incorrect password entered!");
+        }
+
+        return new ObjectResult(GenerateToken(user.Id.ToString()));
     }
 
     // Generate a Token with expiration date and Claim meta-data.
