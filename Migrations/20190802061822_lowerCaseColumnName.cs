5494db0f95aeafba331b2c67f3f258948f3448f8
Tobias Janssen <tobiasjanssen@tobiass-MBP.fritz.box>
2019-08-02 08:47:32 +0200

    Fix lowercase db column

---
+++ Migrations/20190802061822_lowerCaseColumnName.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ColaTerminal.Migrations
+{
+    public partial class lowerCaseColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("Price", "refill");
+            migrationBuilder.AddColumn<double>(
+                name: "price",
+                table: "refill",
+                nullable: false,
+                defaultValue: 0.0);
+            
+            migrationBuilder.DropColumn("Price", "proceed");
+            migrationBuilder.AddColumn<double>(
+                name: "price",
+                table: "proceed",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.DropColumn("Token", "user");
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "user",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "user");
+            
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "proceed");
+            
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "refill");
+        }
+    }
+}
