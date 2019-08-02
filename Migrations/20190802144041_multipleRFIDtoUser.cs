0b6fa3365e39922bce9f4c24d7d003005e2ebbd6
Tobias Janssen <tobias.janssen@traperto.com>
2019-08-02 16:47:08 +0200

    Migrationscript Multipli RFIDs to User Table

---
+++ Migrations/20190802144041_multipleRFIDtoUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ColaTerminal.Migrations
+{
+    public partial class multipleRFIDtoUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("rfid", "user");
+            
+            migrationBuilder.CreateTable(
+                name: "rfid",
+                columns: table => new
+                {
+                    userId = table.Column<uint>(nullable: true),
+                    rfId = table.Column<int>(type: "int(11)", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "rfid_user",
+                        column: x => x.userId,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
