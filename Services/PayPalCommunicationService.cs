e02e1f644bcc12d8502b9b35d0eb43477c6a1cf6
Curly <n.schmidt@traperto.com>
2019-10-25 19:52:36 +0200

    Anpassungen an PayPalService - Umbenennung und Anpassung des Services

--- Services/PayPalCommunicationService.cs
+++ Services/PayPalCommunicationService.cs
@@ -8,17 +8,17 @@ using ColaTerminal.Models.Paypal;
 
 namespace ColaTerminal.Services
 {
-    public class NetworkService
+    public class PayPalCommunicationService
     {
         private const string BasicUrl = "https://api.paypal.com/";
-        private const string AuthUrl = "v1/oauth2/token";
+        private const string AuthEndpoint = "v1/oauth2/token";
 
         private DateTime _invalidationTime;
         private string _accessToken;
 
         private readonly IHttpClientFactory _clientFactory;
 
-        public NetworkService(IHttpClientFactory clientFactory)
+        public PayPalCommunicationService(IHttpClientFactory clientFactory)
         {
             _clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
         }
@@ -28,19 +28,20 @@ namespace ColaTerminal.Services
             if (client == null) throw new ArgumentNullException(nameof(client));
             if (secret == null) throw new ArgumentNullException(nameof(secret));
 
-            var request = new HttpRequestMessage(HttpMethod.Post, BasicUrl + AuthUrl);
-            request.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-
+            // Append Basic-Auth data
             var authToken = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{client}:{secret}"));
             var httpClient = _clientFactory.CreateClient();
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
 
+            // Build form-data
             var formData = new List<KeyValuePair<string, string>>
             {
                 new KeyValuePair<string, string>("grant_type", "client_credentials")
             };
+            var requestBody = new FormUrlEncodedContent(formData);
 
-            var result = await httpClient.PostAsync(BasicUrl + AuthUrl, new FormUrlEncodedContent(formData));
+            // Send request and extract values
+            var result = await httpClient.PostAsync(BasicUrl + AuthEndpoint, requestBody);
             if (!result.IsSuccessStatusCode)
             {
                 throw new Exception("Error while receiving authorization-data");
@@ -57,6 +58,7 @@ namespace ColaTerminal.Services
                 throw new Exception("No access-token given");
             }
 
+            // Update token and validation-time
             _invalidationTime = DateTime.Now.Add(TimeSpan.FromSeconds(content.ExpiresIn));
             _accessToken = content.AccessToken;
         }
